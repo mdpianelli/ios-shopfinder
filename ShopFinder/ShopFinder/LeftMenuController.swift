dda091c974257de59997dd2c55cb8b93312bf607
Marcos Pianelli <mdpianelli@gmail.com>
2015-07-09 00:10:56 +0100

    First commit with new libraries

---
+++ ShopFinder/ShopFinder/LeftMenuController.swift
@@ -0,0 +1,9 @@
+//
+//  LeftMenuController.swift
+//  ShopFinder
+//
+//  Created by Marcos Pianelli on 08/07/15.
+//  Copyright (c) 2015 Barkala Studios. All rights reserved.
+//
+
+import Foundation
