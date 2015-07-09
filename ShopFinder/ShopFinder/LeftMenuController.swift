32ba074be04b31fc860ca7d22d15310f8a9f9ad7
Marcos Pianelli <mdpianelli@gmail.com>
2015-07-09 13:18:08 +0100

    Icons addition

--- ShopFinder/ShopFinder/LeftMenuController.swift
+++ ShopFinder/ShopFinder/LeftMenuController.swift
@@ -6,4 +6,70 @@
 //  Copyright (c) 2015 Barkala Studios. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class LeftMenuController: UIViewController, RESideMenuDelegate, UITableViewDataSource, UITableViewDelegate {
+
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    let titles = ["Home", "Calendar", "Profile", "Settings"]
+    let images = ["IconHome", "IconCalendar", "IconProfile", "IconSettings"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.contentInset = UIEdgeInsetsMake(100, 0,0, 0)
+    }
+    
+
+    //MARK: - UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return titles.count
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1;
+    }
+    
+    func tableView(atableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell") as! UITableViewCell
+        
+        cell.textLabel!.textColor = UIColor.whiteColor()
+        cell.textLabel!.text = titles[indexPath.row];
+        cell.imageView!.image = UIImage(named:images[indexPath.row])
+
+        return cell
+    }
+    
+    
+    //MARK : - UITableViewDelegate
+    
+    func tableView(atableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+        
+        switch(indexPath.row)
+        {
+            case 0 :
+                self.sideMenuViewController.setContentViewController(self.storyboard!.instantiateViewControllerWithIdentifier("firstViewController") as! UIViewController, animated: true)
+                
+            case 1:
+                self.sideMenuViewController.setContentViewController(self.storyboard!.instantiateViewControllerWithIdentifier("secondViewController") as! UIViewController, animated: true)
+                
+            default:break;
+        }
+    
+        self.sideMenuViewController.hideMenuViewController()
+
+    }
+    
+    
+    
+}
--- ShopFinder/ShopFinder/MainRootController.swift
+++ ShopFinder/ShopFinder/MainRootController.swift
@@ -6,4 +6,34 @@
 //  Copyright (c) 2015 Barkala Studios. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class MainRootController:  RESideMenu, RESideMenuDelegate {
+    
+    
+
+    override func awakeFromNib() {
+
+        self.menuPreferredStatusBarStyle = UIStatusBarStyle.LightContent
+        self.contentViewShadowColor = UIColor.blackColor()
+        self.contentViewShadowOffset = CGSizeMake(0,0)
+        self.contentViewShadowOpacity = 0.6
+        self.contentViewShadowRadius = 12
+        self.contentViewShadowEnabled = true
+        self.backgroundImage =  UIImage(named:"Stars")
+        self.delegate = self
+        
+        //setup controllers
+       self.contentViewController = self.storyboard!.instantiateViewControllerWithIdentifier("contentViewController") as! UIViewController
+        
+        self.leftMenuViewController = self.storyboard!.instantiateViewControllerWithIdentifier("leftMenuViewController") as! UIViewController
+        
+        self.rightMenuViewController =
+            self.storyboard!.instantiateViewControllerWithIdentifier("rightMenuViewController") as! UIViewController
+        
+        
+    }
+    
+    
+}
